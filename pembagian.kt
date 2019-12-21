7d556b00c5f2da1336c362decc67be55652c6058
SaefulAS <saepulabdullohs@gmail.com>
2019-12-21 12:55:27 +0700

    tugasku3

---
+++ pembagian.kt
@@ -0,0 +1,10 @@
+fun main() {
+    val a = readLine()?.toFloat() //string
+
+    println("masukkan Nilai b :")
+    val b = readLine()?.toFloat()
+
+    val hasil = a!! / b!!
+
+    print("nilai dari penjumlahan $hasil")
+}
---
+++ perkalian.kt
@@ -0,0 +1,10 @@
+fun main() {
+    val a = readLine()?.toInt() //string
+
+    println("masukkan Nilai b :")
+    val b = readLine()?.toInt()
+
+    val hasil = a!! * b!!
+
+    print("nilai dari penjumlahan $hasil")
+}
